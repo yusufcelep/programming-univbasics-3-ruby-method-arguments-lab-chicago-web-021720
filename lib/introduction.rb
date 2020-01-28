f10e251aec4bd93e45292cfded47215ee60aa238
Yusuf Celep <yusufcelep8@gmail.com>
2020-01-28 11:29:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,7 @@ def introduction_with_language (name, language)
 end
 
 def introduction_with_language_optional (name, language = "Ruby")
-  
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"  
 end
 
 
